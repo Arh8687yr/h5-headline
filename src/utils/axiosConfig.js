b8fafd4d5ef977628f28ca53b835e4dce77a6426
执念 <1250696730@qq.com>
2019-12-26 20:20:59 +0800

    大数字处理

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import JSONbig from 'json-bigint'
 
 // 在多个服务器请求时，baseURL 不同，所以不推荐直接操作axios，而是new一个axios实例
 const instance = axios.create({
@@ -12,8 +13,17 @@ instance.interceptors.request.use(config => {
 
 })
 
-// 响应拦截器 拦截请求返回的error
-instance.interceptors.response.use(config => {
+// 大数字处理：获取到服务器返回的数据，并且是在数据处理之前使用
+// transformResponse 是在响应拦截器之前执行
+instance.defaults.transformResponse = [function (data) {
+  try {
+    return JSONbig.parse(data)
+  } catch (err) {
+    return data
+  }
+}]
 
+// 响应拦截器 拦截请求返回的error
+instance.interceptors.response.use(responses => {
 })
 export default instance
