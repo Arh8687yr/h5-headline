21fe62ec8267342ea04b9b70a82fd1eab45f234a
执念 <1250696730@qq.com>
2020-01-07 15:30:05 +0800

    首页布局完成

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -3,7 +3,7 @@
     <router-view></router-view>
     <!-- 此处是tabbar组件 -->
     <!-- 使用子路由的模式让四个页面共享tabbar组件 -->
-    <van-tabbar route>
+    <van-tabbar route fixed>
       <!-- replace 不记录历史 -->
       <van-tabbar-item to="/" icon="home-o">主页</van-tabbar-item>
       <van-tabbar-item to="/qa" icon="search">问答</van-tabbar-item>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,63 @@
 <template>
   <div>
     <!-- 导航头 -->
-    <van-nav-bar>
+    <van-nav-bar title="搜索框" fixed>
       <van-icon name="search" slot="left" />
       <van-icon name="search" slot="right" />
     </van-nav-bar>
+    <!-- 标签栏 频道列表-->
+    <van-tabs animated>
+      <van-tab v-for="index in 8" :key="index" :title="'标签'+index">
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+          <van-cell v-for="item in list" :key="item" :title="item" />
+        </van-list>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 15; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-tabs {
+  /deep/ .van-tabs__wrap {
+    position: fixed;
+    top: 46px;
+    left: 0;
+    z-index: 100;
+  }
+  /deep/ .van-tabs__content {
+    margin-top: 90px;
+    margin-bottom: 50px;
+  }
+}
 </style>
