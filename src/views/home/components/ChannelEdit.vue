ba3a9d002c8e84b3284c4d09e417f9f8e72c8fa1
执念 <1250696730@qq.com>
2020-01-25 17:23:48 +0800

    编辑/完成模式切换

--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -1,11 +1,26 @@
 <template>
   <van-popup
-  :value="value"
-  @input="$emit('input',$event)"
-  position="bottom"
-  :style="{ height: '80%' }"
+    :value="value"
+    @input="$emit('input',$event)"
+    position="bottom"
+    :style="{ height: '87%' }"
   >
-  弹出层
+    <van-cell icon="cross" @click="$emit('input',false)" />
+    <van-cell title="我的频道" label="点击进入频道">
+      <van-button round type="danger" size="mini" v-show="!isEdit" @click="isEdit=true">编辑</van-button>
+      <van-button round type="danger" size="mini" v-show="isEdit" @click="isEdit=false">完成</van-button>
+    </van-cell>
+    <van-grid>
+      <van-grid-item v-for="item in 8" :key="item" text="文字">
+        <!-- 通过默认的slot default实现的 -->
+        <van-icon slot="icon" class="close-icon" name="close" v-show="isEdit"/>
+      </van-grid-item>
+    </van-grid>
+    <van-cell title="推荐频道" label="点击添加频道" />
+    <van-grid>
+      <van-grid-item v-for="item in 8" :key="item" text="文字" class="grid-item">
+      </van-grid-item>
+    </van-grid>
   </van-popup>
 </template>
 
@@ -20,11 +35,17 @@ export default {
   },
   data () {
     return {
+      // 是否为编辑模式
+      isEdit: false
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .close-icon {
+    position: absolute;
+    right: -47px;
+    top: -15px;
+  }
 </style>
